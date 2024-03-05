08248a15a48832e2f6351bbea5001a63eaf9428b
Junaid Razzaq <142839488+Stayrelax84@users.noreply.github.com>
2024-03-06 03:02:56 +0500

    Add files via upload

---
+++ 02/main.js
@@ -0,0 +1,2 @@
+var personName = "Junaid";
+console.log("\"Hello ".concat(personName, ", would you like to learn some Python today?\""));
---
+++ 02/main.ts
@@ -0,0 +1,3 @@
+let personName: string = "Junaid";
+
+console.log(`"Hello ${personName}, would you like to learn some Python today?"`);
