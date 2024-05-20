79837f32263ee7f922753c0a3685eb402828b9b5
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-05-20 23:45:03 +0500

    Add files via upload

---
+++ 32/main.js
@@ -0,0 +1,5 @@
+// Creating Current User & New User Array
+var currentUsers = ["Junaid", "Asif", "Salman", "Danish", "Farhan"];
+var newUsers = ["Asif", "Saqib", "Farhan", "Shehzad", "Danish"];
+// using forEeach loop function to check user name
+newUsers.forEach();
---
+++ 32/main.ts
@@ -0,0 +1,5 @@
+// Creating Current User & New User Array
+let currentUsers = ["Junaid", "Asif", "Salman", "Danish", "Farhan"];
+let newUsers = ["Asif", "Saqib", "Farhan", "Shehzad", "Danish"];
+
+// using forEeach loop function to check user name
