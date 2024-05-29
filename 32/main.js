7fbcb89b232e96def4affa5535508caed0792656
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-05-30 01:42:18 +0500

    Add files via upload

--- 32/main.js
+++ 32/main.js
@@ -1,5 +1,16 @@
 // Creating Current User & New User Array
 var currentUsers = ["Junaid", "Asif", "Salman", "Danish", "Farhan"];
 var newUsers = ["Asif", "Saqib", "Farhan", "Shehzad", "Danish"];
-// using forEeach loop function to check user name
-newUsers.forEach();
+// convert current users into lower case for case-insensitive
+var currentUsersLowerCase = currentUsers.map(function (user) { return user.toLowerCase(); });
+var newUsersLowerCase = newUsers.map(function (user) { return user.toLocaleLowerCase(); });
+// using for Loop
+for (var _i = 0, newUsersLowerCase_1 = newUsersLowerCase; _i < newUsersLowerCase_1.length; _i++) {
+    var newUsers_1 = newUsersLowerCase_1[_i];
+    if (currentUsersLowerCase.includes(newUsers_1)) {
+        console.log("The username ".concat(newUsers_1, " is already Taken Please enter a new username"));
+    }
+    else {
+        console.log("The username ".concat(newUsers_1, " is Available"));
+    }
+}
--- 32/main.ts
+++ 32/main.ts
@@ -2,4 +2,16 @@
 let currentUsers = ["Junaid", "Asif", "Salman", "Danish", "Farhan"];
 let newUsers = ["Asif", "Saqib", "Farhan", "Shehzad", "Danish"];
 
-// using forEeach loop function to check user name
+// convert current users into lower case for case-insensitive
+let currentUsersLowerCase = currentUsers.map((user) => user.toLowerCase());
+let newUsersLowerCase = newUsers.map((user) => user.toLocaleLowerCase());
+// using for Loop
+for (let newUsers of newUsersLowerCase) {
+  if (currentUsersLowerCase.includes(newUsers)) {
+    console.log(
+      `The username ${newUsers} is already Taken Please enter a new username`
+    );
+  } else {
+    console.log(`The username ${newUsers} is Available`);
+  }
+}
